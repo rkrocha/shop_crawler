aa6b76ee320908053e447f362fb0dfb368566d3e
rkrocha <rafaelkr7@gmail.com>
2021-12-16 01:20:41 -0300

    added Kabum crawler

---
+++ crawlers/crawlers/spiders/kabum_spider.py
@@ -0,0 +1,45 @@
+import scrapy
+import datetime
+from .kabum import KABUM_PAGES
+
+
+class KabumSpider(scrapy.Spider):
+    name = 'kabum'
+    allowed_domains = ['www.kabum.com.br/']
+    start_urls = [key for key in KABUM_PAGES]
+
+    def parse(self, response):
+        date = datetime.datetime.now()
+
+        for product in response.xpath("//div[@id='listingOrdenation']/following::main/div[contains(@class, 'productCard')]/a"):
+            price = product.xpath("./descendant::span[contains(@class, 'priceCard')]/text()").get()
+            if '---' in price:
+                break
+
+            doc = {}
+            doc['shop'] = "Kabum"
+            doc['type'] = KABUM_PAGES[response.url]
+            doc['title'] = product.xpath("./descendant::h2/text()").get()
+            doc['url'] = "www.kabum.com.br" + product.xpath("./@href").get()
+
+            info = {}
+            info['price'] = float(''.join(c for c in price if c.isdigit())) / 100 # mover conversão de tipo para o pipeline
+            info['date'] = date
+
+            doc['info'] = info
+
+            yield doc
+
+"""
+{
+    "shop": "",
+    "type": "",
+    "title": "",
+    "url": "",
+    "info":
+    {
+        "date": datetime.datetime(),
+        "price": 0.0,
+    }
+}
+"""
