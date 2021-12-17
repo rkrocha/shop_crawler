dd74180811b469eb4be02e5fd31b9246f885f161
rkrocha <rafaelkr7@gmail.com>
2021-12-17 08:59:01 -0300

    added folder for config files

---
+++ configs/kabum.py
@@ -0,0 +1,8 @@
+
+KABUM_PAGES = {
+    "https://www.kabum.com.br/hardware/placas-mae?page_number=1&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjM2NS45OCwibWF4IjoxMTE3NS4yOX19&sort=price": "placa-mae",
+    "https://www.kabum.com.br/hardware/processadores?page_number=1&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjM1MC42OSwibWF4IjoxMTk4OS45OX19&sort=price": "cpu",
+    "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=1&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjI5OC4wOSwibWF4IjoyNzY2NC40NX19&sort=price": "gpu",
+    "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=2&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjI5OC4wOSwibWF4IjoyNzY2NC40NX19&sort=price": "gpu",
+    "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=3&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjI5OC4wOSwibWF4IjoyNzY2NC40NX19&sort=price": "gpu",
+}
